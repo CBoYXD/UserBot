c59614e26499cf0811288096982c2137b47f7f3c
CBoYXD <python.rust.cpp@gmail.com>
2024-04-02 14:26:17 +0300

    Deleted unnecessary files

--- src/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .fun import fun_router
-from .ignore import ignore_router
-from .interpreters import intrp_router
-
-routers = [ignore_router, fun_router, intrp_router]
--- src/modules/interpreters.py
+++
@@ -1,7 +0,0 @@
-from ..tools.router import Router
-from pyrogram import filters
-from pyrogram.errors import FloodWait
-from pyrogram.types import Message
-from asyncio import sleep
-
-intrp_router = Router('intrp')
