710bf78b1b72a849e21cb4cd6be9a840c6252128
CBoYXD <python.rust.cpp@gmail.com>
2024-05-16 13:18:51 +0300

    Delete unnecessary file

--- src/services/code_pars/python.py
+++
@@ -1,9 +0,0 @@
-class PythonCode:
-    def __init__(self, code_msg: str):
-        self.__code = self.pars_tg_code(code_msg)
-
-    def pars_tg_code(self, code_msg):
-        pass
-
-    async def run(self) -> str:
-        pass
